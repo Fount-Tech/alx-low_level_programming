e8a717498a9803b7ea63c9ff4c20fc3fbb9ab566
Fount-Tech <fountech@gmail.com>
2022-10-13 10:54:21 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-/*main */	int main(void)
+/**main */ int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n ");
+	puts("Programming is like building a multilingual puzzle\n");
 
 	return (0);
 
