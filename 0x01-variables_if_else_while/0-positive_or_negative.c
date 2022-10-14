ba52b922da07b61f852261574c725de9c1095445
Fount-Tech <fountech@gmail.com>
2022-10-14 10:37:57 -0700

    Program that determin positive and negative numbers

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	printf("Enter a number:");
+	scanf("%d", &n);
+
+	if (n > 0)
+	{
+		printf("is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("is zero\n", n);
+	}
+	else
+	{
+	printf("is negative\n", n);
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+README for if and else project
