4c31275c2635396060130bd6d7e2db6095852364
Fount-Tech <fountech@gmail.com>
2022-10-19 08:25:47 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "mainn.h"
+/**
+ * main - Write a fuction that prints the alphabet, in lowercase,
+ *followed by a new line.
+ */
+void print_alphabet(void);
+{	char alpha;
+	for (alpha = 'a'; <= 'z'; alpha++)
+	_putchar(alpha);
+	_putchar('\n');
+}
