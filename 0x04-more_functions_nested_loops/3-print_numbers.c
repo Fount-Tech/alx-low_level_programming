f95a5adfcf4700de70cb4a3877c05cb559e07868
Fount-Tech <fountech@gmail.com>
2022-12-13 06:39:32 -0800

    correction to print 0 - 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,16 @@
+
 #include "main.h"
+
 /**
- * print_numbers - print the numbers from 0-9
+ * print_numbers - print the numbers from 0 - 9
  *
  * Return: void
  */
 void print_numbers(void)
 {
-	int;
-	for (int i = '0'; i <= '9'; i++)
-		putchar(i);
-		_putchar('\n');
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+		_putchar(i);
+	_putchar('\n');
 }
