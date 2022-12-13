25764201e8d72631ae4b08f19e1f03ec6cb5dfb3
Fount-Tech <fountech@gmail.com>
2022-12-13 06:21:19 -0800

    Add two int

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: the first integer to b multiplied
+ * @b: second integer to be multiplied
+ *
+ * Return: returns the result of the multiplication
+ */
+
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
