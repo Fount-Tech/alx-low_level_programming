9f978b21839f086d06b2e68edea644ada28f5792
Fount-Tech <fountech@gmail.com>
2022-12-13 06:32:48 -0800

    0 - 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_numbers - print the numbers from 0-9
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	for (int i = 0; i <= 9; i++)
+		putchar(i);
+		putchar('\n');
+}
