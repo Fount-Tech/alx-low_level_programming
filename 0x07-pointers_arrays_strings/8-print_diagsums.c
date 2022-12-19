9b62486ecc12947296d5295a5364c4638654fb11
Fount-Tech <fountech@gmail.com>
2022-12-19 11:57:11 -0800

    pointer to char

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,21 +1,32 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * print_chessboard - prints the chessboard
- * @a: array of characters
+ * print_diagsums - prints the sum of the two diagonals of a square
+ * matrix of integers
+ * @a: pointer to the matrix
+ * @size: size of square
  *
  * Return: void
  */
-void print_chessboard(char (*a)[8])
+void print_diagsums(int *a, int size)
 {
-	int i, j;
+	int i, sum1, sum2;
 
-	for (i = 0; i < 8; i++)
+	sum1 = 0;
+	sum2 = 0;
+
+	for (i = 0; i < size * size; i++)
 	{
-		for (j = 0; j < 8; j++)
+		if (i % (size + 1) == 0)
 		{
-			_putchar(a[i][j]);
+			sum1 += a[i];
 		}
-		_putchar('\n');
+		if (i != 0 && i % (size - 1) == 0 && i < (size * size) - size + 1)
+		{
+			sum2 += a[i];
+		}
 	}
+
+	printf("%d, %d\n", sum1, sum2);
 }
